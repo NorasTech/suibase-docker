b0cbdacc3757389e0559fa85a074659fc34151ef
Kerem Noras <kerem@noras.tech>
2024-09-13 12:23:04 +0300

    Refactor Docker setup and update start script in Dockerfile and start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ if [ -n "$FORCE_TAG" ]; then \
   echo 'force_tag: "${FORCE_TAG}"' >> $config
   localnet update
 fi
-localnet start &! \
+localnet start &! && \
 # wait a while
-sleep 15
+sleep 15 && \
 tail -f ~/suibase/workdirs/common/logs/suibase-daemon.log
